2fc2cb7ee90a678960e5fe1b13418e42068a3802
kazunari.ueeda <kazuusjlo8@gmail.com>
2020-05-08 12:27:55 +0900

    Adding a Migration File

---
+++ html/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$dns = 'mysql:dbname=kenshu-back;host=localhost;charset=utf8';
+$root = 'root';
+$pass = 'pass';
+
+try {
+// データベースに接続
+$pdo = new PDO($dns, $root, $pass);
+    echo "success connecting DB";
+
+} catch (PDOException $e) {
+    exit('データベースに接続できませんでした。' . $e->getMessage());
+}
