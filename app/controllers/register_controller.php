d8fccba883e7d49e00ed50691a6a616877f0ffd7
kazunari.ueeda <kazuusjlo8@gmail.com>
2020-05-11 10:00:18 +0900

    Added the controller.

--- app/controllers/register_controller.php
+++ app/controllers/register_controller.php
@@ -7,12 +7,15 @@ require_once dirname(__FILE__) . '/../models/users.php';
 use app\model\users;
 
 class register_controller extends controller {
+    private $route_name = 'register_controller';
+
     public function __construct() { }
 
     public function index() {
-        $users = new users();
-        return $this->view($this->name, [
-            ''
+        $users = new register_controller();
+
+        return $this->view($this->route_name, [
+            'email_address' => $users->getAll(),
         ]);
     }
 
@@ -21,11 +24,12 @@ class register_controller extends controller {
             $param = filter_input(INPUT_POST, 'name',
             FILTER_SANITIZE_SPECIAL_CHARS);
 
-            $this->logging($param);
+            // $this->logging($param);
 
             $users = new users();
             $users->insert([
-                'name' => $param
+                'email_address' => $param,
+                'password' => $param
             ]);
         }
     }
